f8cc7db971e84bcf1c7d9d778e1880d2d41db397
neuroeeg <67147000+neuroeeg@users.noreply.github.com>
2020-09-19 16:38:21 +0400

    Update dataset_generator.py

--- dataset_generator.py
+++ dataset_generator.py
@@ -5,14 +5,16 @@ import os
 
 files = os.listdir()
 files_for_set=[]
+# получаем список названий файлов в директории
 for i in files:
     if (i.endswith('.txt')==1):
         files_for_set.append(i)
 print(files_for_set[0][0:3])
 
 
-#lengh of signal
+#длина сигнала. В секундах это t=sig_len/fd, где fd - частота дискретизации.
 sig_len = 1000
+#количество каналов
 ch_num = 4
 
 
@@ -26,28 +28,33 @@ for i in files_for_set:
     x=[]
     for i in range(ch_num):
         x.append([l[j][i] for j in range(len(l))])
-
+        
+    # отрисовка сигналов для проверки их корректности (необязательна)
     # #figure
     # for i in range(ch_num):
     #     plt.plot(x[i])
     # plt.show()
 
-    #write train set
+    #записываем тренировочный сет
     f = open('train_dataset_set_1000.txt', 'a')
     total_size=int(len(x[0])/sig_len)
     for i in np.arange(0,int(total_size-total_size/5),1):#total_size
         print('set_len write:'+str(i))
         for j in range(ch_num):
+            # добавляем в сет сигналы с каждого канала
             f.write(str(x[j][i*sig_len:(i+1)*sig_len]))
+        # добавляем метку класса
         f.write(', '+file_name[0:3]+'\n')
     f.close()
     
-    #write test set
+    #из неиспользованных для тренировочного сета данных, записывает тестовый сет
     ft= open('test_dataset_set_1000.txt', 'a')
     for i in np.arange(int(total_size-total_size/5),int(total_size),1):#total_size
         print('set_len write:'+str(i))
         for j in range(ch_num):
+            # добавляем в сет сигналы с каждого канала
             ft.write(str(x[j][i*sig_len:(i+1)*sig_len]))
+        # добавляем метку класса
         ft.write(', '+file_name[0:3]+'\n')
     ft.close()
 print('end!')
